0659ecd8f3069ae3da1a612ab34dca68315cc6b1
Oleg <o.sas@aisnovations.com>
2017-01-24 11:57:52 +0300

    initial commit A

---
+++ forRobomongaA.js
@@ -0,0 +1,12 @@
+db.transactions.aggregate([{$match:{"Friend_id":{$exists:true}}},
+                                    {$group:{_id:{friend:"$Friend_id",currency:"$Currency"},Amount:{$sum:"$Amount"}}},
+                                     {$project:{_id:"$_id.friend",currency:"$_id.currency",amount:"$Amount"}},
+                                     {$group:{_id:"$_id",debts:{$push:{currency:"$currency",total:"$amount"}}}},
+                                     {$lookup:{
+                                        from:"friends",
+                                         localField:"_id",
+                                         foreignField:"_id",
+                                         as:"_id"
+                                         }},
+                                      {$project:{"_id":"$_id.name","debts":1}} 
+])
