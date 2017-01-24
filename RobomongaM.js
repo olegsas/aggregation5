be00ab3728dc89753f0299a7e7959d1800cc7715
Oleg <o.sas@aisnovations.com>
2017-01-24 12:51:15 +0300

    final beautiful

--- RobomongaM.js
+++ RobomongaM.js
@@ -1,13 +1,12 @@
-db.transactions.aggregate([{$match:{"Category": "Borrow"}},
-{$lookup:{
-                                        from:"friends",
-                                         localField:"Friend_id",
-                                         foreignField:"_id",
-                                         as:"Friends_Obj"
-                                         }},                         
+db.transactions.aggregate([
+{$match:{"Category": "Borrow"}},
+{$lookup:{from:"friends",
+          localField:"Friend_id",
+          foreignField:"_id",
+          as:"Friends_Obj"
+}},                         
 {$project:{"_id":0, "Amount": 1, "Currency": 1, "Friends_Obj": 1}},
 {$group:{_id:{"friend":"$Friends_Obj.name","currency":"$Currency"},"Amount":{$sum:"$Amount"}}},
 {$project:{_id:"$_id.friend","currency":"$_id.currency","amount":"$Amount"}},
 {$group:{_id:"$_id","debts":{$push:{"currency":"$currency","total":"$amount"}}}}
-//                                      
 ])
